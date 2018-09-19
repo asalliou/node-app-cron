941ece591911fe6fdbf67aed11781baefb0c9ce3
Antoine Salliou <antoine.salliou@gmail.com>
2018-09-19 22:23:10 +0200

    Print current timestamp to file

--- app/app.js
+++ app/app.js
@@ -1,1 +1,7 @@
-console.log("Hi !")
+const fs = require('fs');
+
+const timestamp = Math.floor(Date.now() / 1000);
+
+fs.appendFile('timestamp.txt', timestamp + '\n', function (err) {
+  if (err) throw err;
+});
